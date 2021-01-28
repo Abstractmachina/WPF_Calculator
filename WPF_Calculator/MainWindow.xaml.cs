6638b1899b6b7cc363636e4597527d2972ddcae7
AM_00 <abstractmachina@gmail.com>
2021-01-28 23:42:26 +0000

    implement all math operations.

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -20,11 +20,104 @@ namespace WPF_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+        SelectedOperator selectedOp;
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            label_result.Content = "12123";
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedVal = int.Parse((sender as Button).Content.ToString());
+
+            if (result_label.Content.ToString() == "0")
+            {
+                result_label.Content = $"{selectedVal}";
+            }
+            else result_label.Content = $"{result_label.Content}{selectedVal}";
+        }
+
+
+
+        #region Operations
+        private void Button_comma_Click(object sender, RoutedEventArgs e)
+        {
+            if (result_label.Content.ToString().Contains("."))
+            {
+                //do nothing
+            }
+            else
+            {
+                result_label.Content = $"{result_label.Content}.";
+            }
+            
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(result_label.Content.ToString(), out result))
+            {
+                switch (selectedOp)
+                {
+                    case SelectedOperator.Addition:
+                        result += lastNumber;
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = lastNumber - result;
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result *= lastNumber;
+                        break;
+                    case SelectedOperator.Division:
+                        result = lastNumber / result;
+                        break;
+                    default:
+                        break;
+                }
+
+                result_label.Content = result;
+            }
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(result_label.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                result_label.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(result_label.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                result_label.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            result_label.Content = "0";
+        }
+
+        
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(result_label.Content.ToString(), out lastNumber))
+            {
+                result_label.Content = "0";
+            }
+            if (sender == button_multiplication) selectedOp = SelectedOperator.Multiplication;
+            if (sender == button_division) selectedOp = SelectedOperator.Division;
+            if (sender == button_addition) selectedOp = SelectedOperator.Addition;
+            if (sender == button_subtraction) selectedOp = SelectedOperator.Subtraction;
         }
     }
+
+    #endregion
 }
---
+++ WPF_Calculator/SelectedOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Calculator
+{
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+}
